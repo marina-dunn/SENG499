c56cf94cc22feda5c6c1601766864a93585b8fda
Logan Macdonald <logan.m@live.ca>
2020-07-16 12:29:20 -0700

    Noralized data points, argument specifies directory for data

--- gen_test_data.py
+++ gen_test_data.py
@@ -3,9 +3,11 @@ import sys
 from PIL import Image
 import glob
 
-with open('data.csv','w') as file:
+gesture = sys.argv[1]
+
+with open('{}.csv'.format(gesture),'w') as file:
     face_num = 0;
-    for filename in glob.glob('media/identified_gestures/smile/*.jpg'):
+    for filename in glob.glob('media/identified_gestures/{}/*.jpg'.format(gesture)):
 
         # Load the jpg file into a numpy array
         image = face_recognition.load_image_file(filename)
@@ -19,11 +21,25 @@ with open('data.csv','w') as file:
             face_num += 1
             
             for facial_feature in face_landmarks.keys():
-                file.write("{},{}".format(face_num,facial_feature))
+                if(facial_feature != 'chin' and facial_feature != 'nose_bridge' and facial_feature != 'nose_tip'):
+                    file.write("{},{}".format(face_num,facial_feature))
+                    
+                    norm_x = []
+                    norm_y = []
+                    
+                    max_x = face_landmarks[facial_feature][0][0]
+                    min_x = max_x
+                    max_y = face_landmarks[facial_feature][0][1]
+                    min_y = max_y
+                    
+                    for points in face_landmarks[facial_feature]:
+                        if(max_x < points[0]): max_x = points[0]
+                        elif(min_x > points[0]): min_x = points[0]
+                        
+                        if(max_y < points[1]): max_y = points[1]
+                        elif(min_y > points[1]): min_y = points[1]
+                        
+                    for points in face_landmarks[facial_feature]:
+                        file.write(",{},{}".format( (points[0]-min_x)/(max_x-min_x), (points[1]-min_y)/(max_y-min_y)))
             
-                ref_x = face_landmarks[facial_feature][0][0]
-                ref_y = face_landmarks[facial_feature][0][1]
-                for points in face_landmarks[facial_feature]:
-                    file.write(",{}:{}".format(ref_x-points[0],ref_y-points[1]))
-            
-                file.write("\n")
+                    file.write("\n")
